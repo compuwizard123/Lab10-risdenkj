3c43ec9d16d5e46aa6b37465749039cc237b227d
Kevin Risden <compuwizard123@gmail.com>
2011-05-06 15:55:31 -0400

    implement getting specific fortune

--- Fortune-Team02/fortune.rb
+++ Fortune-Team02/fortune.rb
@@ -7,6 +7,16 @@ class Fortune < Sinatra::Base
     @fortune = $fortunes[rand($fortunes.size)]
     haml :fortune
   end
+
+  get '/fortune/get/:fid' do
+    fid = params[:fid].to_i
+    if fid < 0 || fid > $fortunes.size
+      @fortune = nil
+    else
+      @fortune = $fortunes[fid]
+    end
+    haml :fortune
+  end
 end
 
 require 'fortunedata'
